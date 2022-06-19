133f9ad0e9d20757ca2e832c640dc9abac0e3fa8
Anton Dudakov <anton.dudakov@ziina.com>
2022-06-19 21:56:00 +0300

    remove also tweets with "Promoted by ..." text

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,19 +1,21 @@
+const promotionRegExp = /^Promoted[\s\w]*$/;
+
 const removePromotedTweets = () => {
     elements = document.getElementsByTagName('article');
-    console.log("articles: " + elements.length)
+    console.log("articles: " + elements.length);
 
     for (var i=0; i<elements.length; i++) {
         spans = elements[i].getElementsByTagName('span');
 
         isPromoted = false
         for (j=0; j<spans.length && !isPromoted; j++){
-            if (spans[j].textContent == "Promoted"){
-                console.log("promotion found in: " + elements[i].textContent);
-                isPromoted = true
+            if (promotionRegExp.test(spans[j].textContent)){
+                console.log("promotion found by text \"" + spans[j].textContent + "\" in: " + elements[i].textContent);
+                isPromoted = true;
 
-                elements[i].parentNode.textContent = ""
-                result = elements[i].parentNode.removeChild[elements[i]]
-                console.log("rpmotion deletion result: " + result)
+                elements[i].parentNode.textContent = "";
+                result = elements[i].parentNode.removeChild[elements[i]];
+                console.log("promotion deletion result: " + result);
             }
         }
     }
